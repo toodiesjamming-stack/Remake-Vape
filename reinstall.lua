358fe5f4df58734f1a4dd50e1ca29e67daa0ae47
toodiesjamming-stack <toodiesjamming@gmail.com>
2026-06-02 17:44:41 -0500

    Update reinstall.lua

--- reinstall.lua
+++ reinstall.lua
@@ -1,6 +1,6 @@
 local folderToClean = "newvape"
 local folderToKeep = "profiles"
-local reinstallUrl = "https://raw.githubusercontent.com/poopparty/poopparty/main/NewMainScript.lua"
+local reinstallUrl = "https://raw.githubusercontent.com/toodiesjamming-stack/Remake-Vape/main/NewMainScript.lua"
 
 local function deleteRecursive(path, keepPath)
     if path == keepPath then return end
@@ -32,6 +32,6 @@ for _, item in ipairs(listfiles(folderToClean)) do
     end
 end
 
-print("loading aerov4...")
+print("loading skidv4...")
 task.wait(1) --no no crash dabo
 loadstring(game:HttpGet(reinstallUrl, true))()
